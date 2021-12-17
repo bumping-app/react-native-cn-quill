58d98486a07bd3b623cb844ed2f2bc7265259528
Umang <umangrathore70@gmail.com>
2021-12-17 05:44:53 +0530

    Repo Fixed

--- src/utils/editor-utils.ts
+++ src/utils/editor-utils.ts
@@ -16,9 +16,11 @@ interface CreateHtmlArgs {
   initialHtml?: string;
   placeholder: string;
   toolbar: string;
+  clipboard?: string;
   libraries: 'local' | 'cdn';
   theme: 'snow' | 'bubble';
   editorId: string;
+  autoSize?: boolean;
   containerId: string;
   color: string;
   backgroundColor: string;
@@ -26,21 +28,25 @@ interface CreateHtmlArgs {
   customStyles: string[];
   fonts: Array<CustomFont>;
   defaultFontFamily?: string;
+  customJS?: string;
 }
 
 const Inital_Args = {
   initialHtml: '',
   placeholder: 'write here',
   toolbar: 'false',
+  clipboard: '',
   libraries: 'local',
   theme: 'snow',
   editorId: 'editor-container',
+  autoSize: false,
   containerId: 'standalone-container',
   color: 'black',
   backgroundColor: 'white',
   placeholderColor: 'rgba(0,0,0,0.6)',
   customStyles: [],
   fonts: [],
+  customJS: '',
 } as CreateHtmlArgs;
 
 export const createHtml = (args: CreateHtmlArgs = Inital_Args) => {
@@ -58,6 +64,7 @@ export const createHtml = (args: CreateHtmlArgs = Inital_Args) => {
   ${editor_css(
     args.editorId,
     args.containerId,
+    !!args.autoSize,
     args.color,
     args.backgroundColor,
     args.placeholderColor,
@@ -74,21 +81,23 @@ export const createHtml = (args: CreateHtmlArgs = Inital_Args) => {
       })
       .join('\n')
   }
-  
+
   </head>
   <body>
   <div id="${args.containerId}">
-  <div id="${args.editorId}">
-    ${args.initialHtml}
-  </div>
+    <div id="${args.editorId}">
+      ${args.initialHtml}
+    </div>
   </div>
   ${quill_js(args.libraries === 'cdn')}
   ${create_quill(
     args.editorId,
     args.toolbar,
+    args.clipboard ? args.clipboard : '',
     args.placeholder,
     args.theme,
-    args.fonts.map((f) => getFontName(f.name))
+    args.fonts.map((f) => getFontName(f.name)),
+    args.customJS ? args.customJS : ''
   )}
   ${editor_js}
   </body>
