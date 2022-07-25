26fa976010855430d5d1376d6fca25e2229b58c1
Chris Cheung <Chris@bumping.app>
2022-07-25 20:47:31 +0800

    working with hide area

--- src/toolbar/quill-toolbar.tsx
+++ src/toolbar/quill-toolbar.tsx
@@ -10,7 +10,8 @@ import {
   TouchableOpacity,
   Animated,
   Easing,
-  Image
+  Image,
+  Keyboard
 } from 'react-native';
 import { fullOptions, basicOptions } from '../constants/toolbar-options';
 import type {
@@ -36,7 +37,7 @@ import { ToggleIconButton } from './components/toggle-icon-button';
 import { formatType } from '../constants/formats';
 
 const WIDTH = Dimensions.get('window').width;
-
+const HEIGHT = Dimensions.get('window').height;
 
 
 // export const ToolbarMenu: React.FC<Props> = ({ title, children }) => {
@@ -75,6 +76,9 @@ interface ToolbarState {
   defaultFontFamily?: string;
   showMenu: boolean;
   isAnimating: boolean;
+  keyboardHeight: number;
+  shortHeight: number;
+  normalHeight: number;
 }
 
 export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
@@ -83,8 +87,16 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
   };
 
   animatedValue: Animated.Value;
+  animatedValueOut: Animated.Value;
+  keyboardShowListener;
+  keyboardHideListener;
+  keyboardDidShowListener;
+
   constructor(props: QuillToolbarProps) {
     super(props);
+    this._keyboardDidShow = this._keyboardDidShow.bind(this);
+    this._keyboardWillShow = this._keyboardWillShow.bind(this);
+    this._keyboardDidHide = this._keyboardDidHide.bind(this);
 
     this.state = {
       toolSets: [],
@@ -94,9 +106,13 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
       theme: lightTheme,
       defaultFontFamily: undefined,
       showMenu: false,
-      isAnimating: false
+      isAnimating: false,
+      keyboardHeight: 0,
+      shortHeight: 0,
+      normalHeight: 0
     };
     this.animatedValue = new Animated.Value(0);
+    this.animatedValueOut = new Animated.Value(43);
   }
 
   editor?: QuillEditor;
@@ -105,6 +121,23 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
     this.listenToEditor();
     this.prepareIconset();
     this.changeTheme();
+
+    this.keyboardShowListener = Keyboard.addListener('keyboardWillShow', (e) => this._keyboardWillShow(e));
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', (e) => this._keyboardDidShow(e));
+    this.keyboardHideListener = Keyboard.addListener('keyboardDidHide', (e) => this._keyboardDidHide(e));
+
+
+
+  }
+
+
+  componentWillUnmount(): void {
+    //Keyboard.removeListener('keyboardDidShow', this._keyboardDidShow);
+    // Keyboard.removeAllListeners('keyboardDidShow');
+    // Keyboard.removeAllListeners('keyboardDidHide');
+    this.keyboardShowListener.remove();
+    this.keyboardHideListener.remove();
+    this.keyboardDidShowListener.remove();
   }
 
   componentDidUpdate(prevProps: QuillToolbarProps, prevState: ToolbarState) {
@@ -117,6 +150,34 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
     if (prevProps.theme !== this.props.theme) {
       this.changeTheme();
     }
+  }
+
+
+  _keyboardWillShow = (e) => {
+    this.setState({
+      keyboardHeight: e.endCoordinates.height,
+      normalHeight: Dimensions.get('screen').height,
+      shortHeight: 0,
+    });
+
+  }
+
+  _keyboardDidShow = (e) => {
+    this.setState({
+      keyboardHeight: e.endCoordinates.height,
+      normalHeight: Dimensions.get('screen').height,
+      shortHeight: Dimensions.get('screen').height - e.endCoordinates.height - 100,
+    });
+
+  }
+
+  _keyboardDidHide = (e) => {
+    this.setState({
+      keyboardHeight: e.endCoordinates.height,
+      normalHeight: Dimensions.get('screen').height,
+      shortHeight: 0,
+    }, () => { this.setState({ shortHeight: Dimensions.get('screen').height - e.endCoordinates.height }) });
+
   }
 
   changeTheme() {
@@ -133,7 +194,7 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
   private prepareIconset = () => {
     const { options, optionsAttach, custom } = this.props;
     let toolbarOptions: Array<Array<string | object> | string | object> = [];
-    
+
     if (options === 'full' || options === []) {
       toolbarOptions = fullOptions;
     } else if (options === 'basic') {
@@ -181,20 +242,22 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
     this.editor?.format(name, value);
   };
 
-  show = (menu:string) => {
+  show = (menu: string) => {
     if (this.state.isAnimating) return;
 
-    this.setState({menuType: menu});
+    this.setState({ menuType: menu });
     const { theme } = this.props;
     if (theme) {
       this.setState({ isAnimating: true }, () => {
-        Animated.timing(this.animatedValue, {
-          toValue: 350, // 2 * theme.size + 14,
-          duration: 200,
-          easing: Easing.linear,
-          useNativeDriver: false,
-        }).start(() => this.setState({ showMenu: true, isAnimating: false }));
-      });
+       
+          Animated.timing(this.animatedValue, {
+            toValue: 0.4 * HEIGHT, // 2 * theme.size + 14,
+            duration: 200,
+            easing: Easing.linear,
+            useNativeDriver: false,
+          }).start(() => this.setState({ showMenu: true, isAnimating: false }));
+        });
+
     }
   };
 
@@ -209,6 +272,7 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
           easing: Easing.linear,
           useNativeDriver: false,
         }).start(() => {
+
           this.setState({
             showMenu: false,
             isAnimating: false,
@@ -217,6 +281,9 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
       });
     }
   };
+
+
+
 
 
 
@@ -260,108 +327,108 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
 
 
           {/* <View style={toolbarStyle}> */}
-          <TouchableOpacity style={{width:WIDTH,  borderWidth:0, marginBottom:0}} onPress={this.hide}>
-          <Animated.View pointerEvents={'box-none'} style={{borderWidth:0, height:'100%', flexDirection:'column', alignItems:'flex-end' ,justifyContent:'flex-end' }}>
-            {/* This scrollView is for the main toolbar */}
+          <TouchableOpacity style={{ width: WIDTH, borderWidth: 0, marginBottom: 0 }} onPress={this.hide}>
+            <Animated.View pointerEvents={'box-none'} style={{ borderWidth: 0, marginBottom: 0, flex: 1, height:this.state.showMenu ? 0.6*HEIGHT : null, flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+              {/* This scrollView is for the main toolbar */}
 
-            {this.state.isAnimating || this.state.showMenu ?
-              <Animated.ScrollView
-                horizontal={false}
-                bounces={false}
-                showsHorizontalScrollIndicator={false}
-                style={{ width: 300, margin:10, paddingLeft:10, paddingRight:10,  borderWidth: 0.25, borderRadius: 10, backgroundColor: 'rgba(255,255,255,1)', maxHeight: this.animatedValue }}
-              >
-                
-                {toolSetsSelected.map((object, index) => {
+              {this.state.isAnimating || this.state.showMenu ?
+                <Animated.ScrollView
+                  horizontal={false}
+                  bounces={false}
+                  showsHorizontalScrollIndicator={false}
+                  style={{ width: 300, margin: 10, paddingLeft: 10, paddingRight: 10, borderWidth: 0.25, borderRadius: 10, backgroundColor: 'rgba(255,255,255,1)', maxHeight: this.animatedValue }}
+                >
 
-                  console.log('looping', JSON.stringify(object));
-                  return (
-                  object.length > 0 && object.map((grp) => {
+                  {toolSetsSelected.map((object, index) => {
 
-                    if (grp.name === 'separator') {
-                      return (
-                        <View style={{height:2, backgroundColor:'rgba(0,0,0,0.1)'}} />
-                      )
-                    }
-                    
-                    else if (grp.type === formatType.toggle) {
+                    console.log('looping', JSON.stringify(object));
+                    return (
+                      object.length > 0 && object.map((grp) => {
 
-                      return (
+                        if (grp.name === 'separator') {
+                          return (
+                            <View style={{ height: 2, backgroundColor: 'rgba(0,0,0,0.1)' }} />
+                          )
+                        }
 
-                        <ToggleIconButton
-                          key={index}
-                          name={grp.name}
-                          source={grp.source}
-                          valueOff={grp.valueOff}
-                          valueOn={grp.valueOn}
-                        />
-                      )
-                    } else {
-                      return (
-                        object.length > 0 &&
-                        grp.values?.map((item:any, index:number) => {
-                          console.log('looping 2', grp.name, JSON.stringify(item));
-                          if (
-                            item.type === formatType.color &&
-                            item.valueOn !== true &&
-                            typeof item.valueOn !== 'number'
-                          ) {
-                            return (
-                              <ToggleColorButton
-                                key={index}
-                                name={grp.name}
-                                valueOff={false}
-                                valueOn={item.valueOn}
-                              />
-                            );
-                          } else if (item.type === formatType.icon) {
-                            return (
-                              <ToggleIconButton
-                                key={index}
-                                source={item.source}
-                                name={grp.name}
-                                valueOff={false}
-                                valueOn={item.valueOn}
-                              />
-                            );
-                          } else
-                            return (
-                              <ToggleTextButton
-                                key={index}
-                                name={grp.name}
-                                valueOff={false}
-                                valueOn={item.valueOn}
-                                valueName={item.name}
-                              />
-                            );
-                        })
-                      )
-                    }
+                        else if (grp.type === formatType.toggle) {
 
-                  }))
+                          return (
 
-                })}
+                            <ToggleIconButton
+                              key={index}
+                              name={grp.name}
+                              source={grp.source}
+                              valueOff={grp.valueOff}
+                              valueOn={grp.valueOn}
+                            />
+                          )
+                        } else {
+                          return (
+                            object.length > 0 &&
+                            grp.values?.map((item: any, index: number) => {
+                              console.log('looping 2', grp.name, JSON.stringify(item));
+                              if (
+                                item.type === formatType.color &&
+                                item.valueOn !== true &&
+                                typeof item.valueOn !== 'number'
+                              ) {
+                                return (
+                                  <ToggleColorButton
+                                    key={index}
+                                    name={grp.name}
+                                    valueOff={false}
+                                    valueOn={item.valueOn}
+                                  />
+                                );
+                              } else if (item.type === formatType.icon) {
+                                return (
+                                  <ToggleIconButton
+                                    key={index}
+                                    source={item.source}
+                                    name={grp.name}
+                                    valueOff={false}
+                                    valueOn={item.valueOn}
+                                  />
+                                );
+                              } else
+                                return (
+                                  <ToggleTextButton
+                                    key={index}
+                                    name={grp.name}
+                                    valueOff={false}
+                                    valueOn={item.valueOn}
+                                    valueName={item.name}
+                                  />
+                                );
+                            })
+                          )
+                        }
 
-          
+                      }))
 
-              </Animated.ScrollView> : null}
-              
+                  })}
 
-              
-            <View style={{ flexDirection:'row',alignItems:'center', justifyContent:'flex-end', height: 40, width: 100, borderWidth: 0, backgroundColor: 'rgba(255,255,255,0)' }}>
-              
-            <TouchableOpacity onPress={() => { this.state.showMenu ? this.hide() : this.show('attach') }}>
-                
-                <Image style={{ padding:5,height:30, width:33}} source={require('./components/Attach.png')} />
-              </TouchableOpacity>
-              
-              <TouchableOpacity onPress={() => { this.state.showMenu ? this.hide() : this.show('format') }}>
-                
-                <Image style={{padding:10, marginLeft:10, marginRight:10, height:27, width:38}} source={require('./components/Format.png')} />
-              </TouchableOpacity>
-              
-            </View>
-          </Animated.View>
+
+
+                </Animated.ScrollView> : null}
+
+
+
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', height: 40, width: 100, borderWidth: 0, backgroundColor: 'rgba(255,255,255,0)' }}>
+
+                <TouchableOpacity onPress={() => { this.state.showMenu ? this.hide() : this.show('attach') }}>
+
+                  <Image style={{ padding: 5, height: 30, width: 33 }} source={require('./components/Attach.png')} />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => { this.state.showMenu ? this.hide() : this.show('format') }}>
+
+                  <Image style={{ padding: 10, marginLeft: 10, marginRight: 10, height: 27, width: 38 }} source={require('./components/Format.png')} />
+                </TouchableOpacity>
+
+              </View>
+            </Animated.View>
           </TouchableOpacity>
         </ToolbarProvider>
       </>
