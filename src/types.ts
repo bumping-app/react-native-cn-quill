ea2a8df48f0175d9bcce7586eae61611539ec1d7
Chris Cheung <Chris@bumping.app>
2022-08-29 02:19:45 +0800

    added callback to types

--- src/types.ts
+++ src/types.ts
@@ -69,7 +69,7 @@ export interface SampleArray {
 }
 
 export interface ToolbarCustom {
-  handler?: (name: string, value: any) => void;
+  handler?: (name: string, value: any, callback: ()=>{}) => void;
   actions?: Array<string>;
   icons?: Record<string, any>;
 }
