24201a18638957e06f03f7bff78a0675f07bc6b4
Chris Cheung <Chris@bumping.app>
2022-08-30 14:13:21 +0800

    Bug transparent

--- src/constants/editor/editor_css.ts
+++ src/constants/editor/editor_css.ts
@@ -34,7 +34,7 @@ export const editor_css = (
 <style>
 * {outline: 0px solid transparent;-webkit-tap-highlight-color: rgba(0,0,0,0);-webkit-touch-callout: none;box-sizing: border-box;}
 html, body { margin: 0; padding: 0; height: 100%;}
-body { overflow-y: hidden; -webkit-overflow-scrolling: touch;background-color: ${background};}
+body { overflow-y: hidden; -webkit-overflow-scrolling: touch;background-color: transparent;}
 #${containerId} {
   color: ${color};
   ${
